f4922852ce83b7e52c4e731597187481fd3de15d
adello <adello3001@naver.com>
2021-04-27 16:45:41 +0900

    [Inheritance] casting: Casting Examples

---
+++ Inheritance/src/casting/Truck.java
@@ -0,0 +1,7 @@
+package casting;
+
+public class Truck {
+	public void run() {
+		System.out.println("트럭이 달린다.");
+	}
+}
---
+++ Inheritance/src/drivers2/Bus.java
@@ -0,0 +1,16 @@
+package drivers2;
+
+public class Bus extends Vehicle {
+	int station;
+	
+	@Override
+	public void run() {
+		System.out.println("버스가 달린다.");
+		station++;
+	}
+	
+	int getStation() {
+		return station;
+		
+	}
+}
