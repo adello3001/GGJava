19f0eeff37e139c654d8d44e352049a785977be2
adello <adello3001@naver.com>
2021-04-29 17:12:08 +0900

    [Inheritance] abstract: Animal

---
+++ Inheritance/src/abstracts2/TigerExample2.java
@@ -0,0 +1,18 @@
+package abstracts2;
+
+public class TigerExample2 {
+
+	public static void main(String[] args) {	
+		Animal animal = new Cat();
+		animal.breathe();
+
+		Cat cat = (Cat)animal;
+		cat.breathe();
+		
+		// 부모 객체를 자식 객체로 캐스팅을 하면
+		// 실행 오류가 발생한다.
+		Tiger tiger = (Tiger)animal;
+		tiger.breathe();
+		
+	}
+}
