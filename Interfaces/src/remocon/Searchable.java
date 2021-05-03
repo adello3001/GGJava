6e62da0682d51fd557783600979b04ae8dcc6621
adello <adello3001@naver.com>
2021-05-03 11:35:18 +0900

    [Interface] remocon: 다중 인터페이스 구현 예제

--- Interfaces/src/remocon/Searchable.java
+++ Interfaces/src/remocon/Searchable.java
@@ -2,4 +2,5 @@ package remocon;
 
 public interface Searchable {
 	void search(String url);
+	default void find(String text) {}
 }
---
+++ Interfaces/src/remocon/Searcher.java
@@ -0,0 +1,15 @@
+package remocon;
+
+public class Searcher implements Searchable {
+
+	@Override
+	public void search(String url) {
+		System.out.printf("Searcher:search(%s)\n", url);
+	}
+	
+	public static void main(String[] args) {
+		Searcher search = new Searcher();
+		search.search("http://naver.com");
+	}
+
+}
